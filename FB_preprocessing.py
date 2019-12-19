b019cca37d4fe6377d809802f4b69fd66d7c52dd
10Exahertz <34243218+10Exahertz@users.noreply.github.com>
2019-12-19 18:05:18 -0500

    Update FB_preprocessing.py

--- FB_preprocessing.py
+++ FB_preprocessing.py
@@ -7,22 +7,23 @@ Created on Fri Nov 15 10:35:36 2019
 """
 from nltk.tokenize import word_tokenize
 #text = "Yea, so the first half you have this mysterious setting where it’s filled with mystery. Then the second half we find out exactly what is talking to him, which I think with certain dialogue we can make the reader know it’s AI without telling them. Also, he def shouldn’t be the one who made it. Cause like how does it go from him creating nova to all of a sudden they’re totally sentient and taking notes on whether to kill him or not. And robot kills its creator has been done too many times. Imo it’s way more interesting that hes just a regular software guy that’s now obsolete. Cause with us not knowing who/when these things were created it’s like oh shit maybe they’ve been doing this for years, going to ppl and killing/maybe even replacing them. -you wouldn’t say any of this in the story but it lets the reader imagine that if you don’t explain everything too much"
-f= open("Rina/Rina_messages.txt","r")
+f= open("input.txt","r")
 contents = f.readlines()
+User = "name"
 #print(contents)
 sentence_1 = []
 i = 0
 for z in range(len(contents)):
     i += 1
-    if contents[i] == "Rina Wang\n":
+    if contents[i] == "User\n":
         string_print = str(contents[i+1])
         quartent = 1
-        if contents[i] == "Rina Wang\n" and contents[i+3] == "Rina Wang\n":
+        if contents[i] == "User\n" and contents[i+3] == "User\n":
             string_4 = str(contents[i+4])
             string_4 = string_4.replace("\n","")
             string_print = string_4+", "+str(contents[i+1])
             quartent = 2
-            if contents[i] == "Rina Wang\n" and contents[i+3] == "Rina Wang\n" and contents[i+6] == "Rina Wang\n":
+            if contents[i] == "User\n" and contents[i+3] == "User\n" and contents[i+6] == "User\n":
                 string_4 = str(contents[i+4])
                 string_4 = string_4.replace("\n","")
                 
@@ -30,7 +31,7 @@ for z in range(len(contents)):
                 string_7 = string_7.replace("\n","")
                 string_print = string_7+", "+string_4+", "+str(contents[i+1])
                 quartent = 3
-                if contents[i] == "Rina Wang\n" and contents[i+3] == "Rina Wang\n" and contents[i+6] == "Rina Wang\n" and contents[i+9] == "Rina Wang\n":
+                if contents[i] == "User\n" and contents[i+3] == "User\n" and contents[i+6] == "User\n" and contents[i+9] == "User\n":
                     string_4 = str(contents[i+4])
                     string_4 = string_4.replace("\n","")
                     
@@ -41,7 +42,7 @@ for z in range(len(contents)):
                     string_10 = string_10.replace("\n","")
                     string_print = string_10+", "+string_7+", "+string_4+", "+str(contents[i+1])
                     quartent = 4
-                    if contents[i] == "Rina Wang\n" and contents[i+3] == "Rina Wang\n" and contents[i+6] == "Rina Wang\n" and contents[i+9] == "Rina Wang\n" and contents[i+12] == "Rina Wang\n":
+                    if contents[i] == "User\n" and contents[i+3] == "User\n" and contents[i+6] == "User\n" and contents[i+9] == "User\n" and contents[i+12] == "User\n":
                         string_4 = str(contents[i+4])
                         string_4 = string_4.replace("\n","")
                         
@@ -56,7 +57,7 @@ for z in range(len(contents)):
                         string_print = string_13+", "+string_10+", "+string_7+", "+string_4+", "+str(contents[i+1])
                         quartent = 5
 
-                        if contents[i] == "Rina Wang\n" and contents[i+3] == "Rina Wang\n" and contents[i+6] == "Rina Wang\n" and contents[i+9] == "Rina Wang\n" and contents[i+12] == "Rina Wang\n" and contents[i+15] == "Rina Wang\n":
+                        if contents[i] == "User\n" and contents[i+3] == "User\n" and contents[i+6] == "User\n" and contents[i+9] == "User\n" and contents[i+12] == "User\n" and contents[i+15] == "User\n":
                             string_4 = str(contents[i+4])
                             string_4 = string_4.replace("\n","")
                             
@@ -103,7 +104,7 @@ for message in sentence_1:
 for line in output_list:
     print(line)
     continue
-outF = open("Rina_setup/Rina_full.txt", "w")
+outF = open("output.txt", "w")
 for line in output_list:
   # write line to output file
   outF.write(line)
